e865cba2ac84b7d875712a705b103112d33b26b3
Alex Wu <alexwu@Alexs-MacBook-Pro.local>
2020-07-14 11:25:15 -0500

    Added a back button

--- yourshare/src/pages/CommunityPage.tsx
+++ yourshare/src/pages/CommunityPage.tsx
@@ -9,7 +9,11 @@ export class CommunityPage extends React.Component<CommunityScreenProps> {
   render() {
     return (
       <div>
+        <p className="App-link" onClick={(e) => this.props.changePage(pages.BrowsePage)}>
+          Back to browse
+        </p>
         <h1>YourShare Manage Community</h1>
+        
       </div>
     );
   }
