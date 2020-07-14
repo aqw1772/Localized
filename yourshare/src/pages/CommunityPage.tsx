a52c431c75b00f93e15fe1d2ff0df353716a000e
Alex Wu <alexwu@Alexs-MacBook-Pro.local>
2020-07-14 11:49:19 -0500

    added a table but not complete

--- yourshare/src/pages/CommunityPage.tsx
+++ yourshare/src/pages/CommunityPage.tsx
@@ -13,7 +13,26 @@ export class CommunityPage extends React.Component<CommunityScreenProps> {
          Back to browse
         </p>
         <h1>YourShare Manage Community</h1>
-        
+        <table>
+          <tr>
+            <th>Friends</th>
+            <th>Item currently borrowed</th>
+            <th>Item currently lent</th>
+            <th>Best friend</th>
+          </tr>
+          <tr>
+            <td>Stacey</td>
+            <td>None</td>
+            <td>None</td>
+            <td><input type="checkbox"></input></td>
+          </tr>
+          <tr>
+            <td>Bob</td>
+            <td>None</td>
+            <td>None</td>
+            <td><input type="checkbox"></input></td>
+          </tr>
+        </table>
       </div>
     );
   }
