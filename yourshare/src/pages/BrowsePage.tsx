7065d1ed4b77bbed7722f8c36fcfad23650e403e
Bunmi Ogidan <bunmiogidan@Bunmis-MBP.fios-router.home>
2020-07-14 12:57:00 -0400

    added table data for first table

--- yourshare/src/pages/BrowsePage.tsx
+++ yourshare/src/pages/BrowsePage.tsx
@@ -9,7 +9,7 @@ export class BrowsePage extends React.Component<BrowseScreenProps> {
   render() {
     return (
     <div>
-        <h1>YourShare Browse</h1>
+        <h1>Welcome, [username]!</h1>
         <p onClick={(e) => this.props.changePage(pages.AddItemPage)}>
           Add item
         </p>
@@ -23,11 +23,51 @@ export class BrowsePage extends React.Component<BrowseScreenProps> {
             <th>Items for borrowing</th>
             <th>Lender</th>
           </tr>
+            <tr>`
+              <td>Blender</td>
+              <td>Stacey</td>
+            </tr>
+            <tr>
+              <td>Rake</td>
+              <td>Marcos</td>
+            </tr>
+            <tr>
+              <td>Car</td>
+              <td>Marcos</td>
+            </tr>
+            <tr>
+              <td>Bike</td>
+              <td>Cam</td>
+            </tr>
+            <tr>
+              <td>Drill</td>
+              <td>Stacey</td>
+            </tr>
+            <tr>
+              <td>Ladder</td>
+              <td>Marcos</td>
+            </tr>
+            <tr>
+              <td>Kayak</td>
+              <td>Cam</td>
+            </tr>
+            <tr>
+              <td>The Office DVD Box set</td>
+              <td>Stacey</td>
+            </tr>
+            <tr>
+              <td>Cart</td>
+              <td>Jim</td>
+            </tr>
           {/* second table on right */}
           <tr>
             <th>Your items</th>
             <th>Lent to</th>
           </tr>
+          {/* <tr> */}
+              {/* <td>Add your first item</td> */}
+              {/* <td>A friend</td> */}
+            {/* </tr> */}
         </table>
 
 
