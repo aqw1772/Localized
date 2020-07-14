dd9528dd4b569d9c638435f427479c05cd6bd4e6
Angel Velazquez <v-angvel@microsoft.com>
2020-07-14 12:01:40 -0400

    Added basic elements of the sign-in page

--- yourshare/src/pages/AccountPage.tsx
+++ yourshare/src/pages/AccountPage.tsx
@@ -9,10 +9,24 @@ export class AccountPage extends React.Component<AccountScreenProps> {
   render() {
     return (
       <div>
-        <h1>YourShare Account</h1>
-        <button onClick={(e) => this.props.changePage(pages.BrowsePage)}>
-          Join Hello
-        </button>
+        <div>
+          <h1>YourShare</h1>
+        </div>
+
+        <div>
+          <h1>Join your community</h1>
+          <h2>Sign-up</h2>
+          <form>
+            <input type="text" placeholder="Username:"/>
+            <input type="text" placeholder="Phone number:"/>
+            <input type="text" placeholder="Zip code:"/>
+
+            {/* <input type="submit" /> */}
+            <button onClick={(e) => this.props.changePage(pages.BrowsePage)}>
+              Join
+            </button>
+          </form>
+        </div>
         <p className="App-link" onClick={(e) => this.props.changePage(pages.BrowsePage)}>
           Sign-in
         </p>
