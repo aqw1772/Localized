8cc9cfd69f34da2b8bb3a51d36ee263990601657
Esther <esther@Esthers-MBP.hsd1.tx.comcast.net>
2020-07-23 14:11:44 -0500

    removed break between image and buttons. nested button in span tag

--- yourshare/src/pages/BorrowItemPage.tsx
+++ yourshare/src/pages/BorrowItemPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { pages } from "../App";
 import YS_blender from "./YS_blender.png";
+import Icon_ArrowLeftCircle from "./icon_ArrowLeftCircle_.png";
 
 
 
@@ -13,7 +14,11 @@ export class BorrowItemPage extends React.Component<BorrowItemScreenProps> {
     return (
       <div className="wrapper"> 
           <div>
-          <h3>Borrow item</h3>
+            {/* Add back button (icon) and title at the top of the page */}
+            <div className="back-button_title">
+              <img src={Icon_ArrowLeftCircle} alt="" onClick={(e) => this.props.changePage(pages.BrowsePage)}/>
+              <h1>Borrow Item</h1>
+            </div>
             <form action="">
               <input type = "text" name ="name" placeholder="Item name: Blender" className="name" readOnly/>
               <br/>
@@ -22,17 +27,25 @@ export class BorrowItemPage extends React.Component<BorrowItemScreenProps> {
               <input type = "text" name ="name" placeholder="Lender: Stacey" readOnly/>
               <br></br>
               <br/>
-              <textarea rows={10} cols={30} readOnly>A pretty great Blender. The lid is a little loose so make
-                  sure to hold it down when using to avoid a smoothie disaster
+              <textarea rows={15} cols={50} readOnly>A pretty great Blender. The lid is a little loose so make
+                  sure to hold it down when using to avoid a smoothie disaster.
               </textarea>
             </form>
           </div>
           
+             
        
             <div>
-            <img src={YS_blender} alt=""></img>
-                <h4>Cancel</h4>
-                <button className="borrow">Request to borrow</button>
+                <div className="container">
+                   <img src={YS_blender} alt="blender image" />
+                 </div>
+                 
+                 <span>
+                   <label className="cancel" onClick={(e) => this.props.changePage(pages.BrowsePage)}>Cancel</label>
+                    <button>Request to Borrow</button>
+                 </span>
+                
+                
             </div>
       </div>
 
