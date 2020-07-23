e163c276a2e4358e759c2c46b29fd590e72b84ac
Bunmi Ogidan <bunmiogidan@Bunmis-MBP.fios-router.home>
2020-07-23 10:23:24 -0400

    added Marcos's last item and finished table

--- yourshare/src/redux/reducer.tsx
+++ yourshare/src/redux/reducer.tsx
@@ -18,14 +18,15 @@ person1.addItem(2, "Blender", "Kitchen", "A pretty great blender.  The lid...");
 const person2 = new Person(3, "Marcos", "206-7654-321", "98115")
 person2.addItem(4, "Rake", "Garden", "A pretty great rake.  The handle...");
 person2.addItem(5, "Car", "Garden", "A pretty great car.  The steering wheel...");
+person2.addItem(6,"Ladder", "A pretty great ladder", "The handle is...");
 
-const person3 = new Person(6, "Cam", "304-266-8178", "83045")
-person3.addItem(7, "Bike", "Garage", "A pretty great bike. The handle is...");
-person3.addItem(8, "Kayak", "Garage","A pretty great kayak. The handle is ...");
+const person3 = new Person(7, "Cam", "304-266-8178", "83045")
+person3.addItem(8, "Bike", "Garage", "A pretty great bike. The handle is...");
+person3.addItem(9, "Kayak", "Garage","A pretty great kayak. The handle is ...");
 
-const person4 =new Person(9, "Stacey", "264-305-7898", "33115")
-person4.addItem(10, "Drill", "Kitchen", "A pretty great drill. The chuck is ...");
-person4.addItem(11, "The office DVD Box set", "Garage", "A pretty great box set. The set is ...");
+const person4 =new Person(10, "Stacey", "264-305-7898", "33115")
+person4.addItem(11, "Drill", "Kitchen", "A pretty great drill. The chuck is ...");
+person4.addItem(12, "The office DVD Box set", "Garage", "A pretty great box set. The set is ...");
 
 const person5 = new Person(13, "Jim", "962-555-3210", "82501")
 person5.addItem(14, "Cart", "Kitchen", "A pretty great cart.  The cart is....");
