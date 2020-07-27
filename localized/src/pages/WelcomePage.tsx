bebae7481f241762f20d5c6b535ac502b39a7375
System Administrator <root@Anthonys-Work-MacBook-Pro.local>
2020-07-27 17:00:16 -0400

    Imported images and added them to WelcomePage.tsx

--- localized/src/pages/WelcomePage.tsx
+++ localized/src/pages/WelcomePage.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { pages } from "../App";
+import logo from "../images/logo.png";
+import iPhone_screenshot from "../images/iPhone.png";
 
 interface WelcomeScreenProps {
   changePage: (page: pages) => void;
@@ -13,10 +15,12 @@ export class WelcomePage extends React.Component<WelcomeScreenProps> {
   render() {
     return (
       <div>
+        <img src={logo}></img>
           <button className="App-link" onClick={(e) => this.props.changePage(pages.BubblePage)}>
             Welcome Page
           </button>
-    </div>
+        <img src={iPhone_screenshot}></img>
+      </div>
     );
   }
 }
