6565630681d90bd193f8cfb1bd0f5612eb56503b
Angel Velazquez <v-angvel@microsoft.com>
2020-07-27 15:12:45 -0400

    Quick push for folders

--- localized/src/components/BubbleList.tsx
+++ localized/src/components/BubbleList.tsx
@@ -3,23 +3,23 @@ import React from 'react';
 // BubbleList component
 // Render the bubbles together for the user to be able to select (for use on the bubbleUI screen)
 
-interface BubbleListProps {
-    listOfBubbles: Array<Bubble>
-}
+// interface BubbleListProps {
+//     listOfBubbles: Array<Bubble>
+// }
 
-class BubbleList extends React.Component<BubbleListProps> {
-    render() {
-        return (
-            <div className="BubbleList" >
-                <ul>
-                    {this.props.listOfBubbles.map((bubble: Bubble) => {
-                            return (
-                                <li>{bubble.name}</li>
-                            )
-                        }
-                    )}
-                </ul>
-            </div >
-        )
-    };
-}
+// class BubbleList extends React.Component<BubbleListProps> {
+//     render() {
+//         return (
+//             <div className="BubbleList" >
+//                 <ul>
+//                     {this.props.listOfBubbles.map((bubble: Bubble) => {
+//                             return (
+//                                 <li>{bubble.name}</li>
+//                             )
+//                         }
+//                     )}
+//                 </ul>
+//             </div >
+//         )
+//     };
+// }
