668f52377504c0331854f4df37778805ecc59949
Angel Velazquez <v-angvel@microsoft.com>
2020-07-27 12:36:23 -0400

    Created basic BubbleList componenet without a Bubble componrnet defined

---
+++ localized/src/components/BubbleList.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// BubbleList component
+// Render the bubbles together for the user to be able to select (for use on the bubbleUI screen)
+
+interface BubbleListProps {
+    listOfBubbles: Array<Bubble>
+}
+
+class BubbleList extends React.Component<BubbleListProps> {
+    render() {
+        return (
+            <div className="BubbleList" >
+                <ul>
+                    {this.props.listOfBubbles.map((bubble: Bubble) => {
+                            return (
+                                <li>{bubble.name}</li>
+                            )
+                        }
+                    )}
+                </ul>
+            </div >
+        )
+    };
+}
