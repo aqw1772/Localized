b895c5c15396d1176c31f28afffbee096871af50
Alex Wu <alexwu@Alexs-MacBook-Pro.local>
2020-07-28 13:48:00 -0500

    I think I completed the bubble component

--- localized/src/components/Bubble.tsx
+++ localized/src/components/Bubble.tsx
@@ -1,1 +1,18 @@
 import React from "react";
+
+
+interface BubbleProps{
+    name: string
+}
+
+class Bubble extends React.Component<BubbleProps>{
+    render(){
+        return(
+            <div>
+                <h1>{this.props.name}</h1>
+            </div>
+        )
+    }
+}
+
+export default Bubble;
