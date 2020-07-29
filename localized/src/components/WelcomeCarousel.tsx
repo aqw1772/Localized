30e42b02b1497fe4de663e618d3e2b67f90cc4f6
System Administrator <root@Anthonys-Work-MacBook-Pro.local>
2020-07-29 16:14:05 -0400

    Downloaded bootstrap and react-bootstrap; initialized carousel component

---
+++ localized/src/components/WelcomeCarousel.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+class WelcomeCarousel extends React.Component<any> { //Need to determine what should go here
+    render() {
+        return (
+            <div className="welcomeCarousel">
+                
+            </div>
+        )
+    };
+}
+
+export default WelcomeCarousel;
