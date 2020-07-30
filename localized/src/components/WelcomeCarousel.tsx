534e26788804cd95d2855516985fb0b1e99132c2
System Administrator <root@Anthonys-Work-MacBook-Pro.local>
2020-07-30 13:15:26 -0400

    Overwrote bult-in react-bootstrap CSS to make indicators circles; changed colors

--- localized/src/components/WelcomeCarousel.tsx
+++ localized/src/components/WelcomeCarousel.tsx
@@ -6,16 +6,35 @@ class WelcomeCarousel extends React.Component {
     render() {
         return (
             <div id="welcomeCarousel">
-                <Carousel>
+                <Carousel fade={true} controls={false}>
+                    {/* <ol className="carousel-indicators">
+                        <li data-slide-to="0" className="active"></li>
+                        <li data-slide-to="1"></li>
+                    </ol> */}
                     <Carousel.Item>
-                        <h2>Get Localized!</h2>
-                        <h4>Welcome to Localized! A web app that allows anyone with a computer or mobile device to 
+                        <h1>Get Localized!</h1>
+                        <br></br>
+                        <h3>Welcome to Localized! A web app that allows anyone with a computer or mobile device to 
                             easily access any information they might need to feel confident enough to participate 
                             in their local government. We have an integrated <b>Civil Supporter</b> chatbot that is 
-                            here to help too!</h4>
+                            here to help too!</h3>
+                        <br></br>
+                        <br></br>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <h4>Second Slide</h4>
+                        <h3>Second Slide</h3>
+                        <br></br>
+                        <br></br>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <h3>Third Slide</h3>
+                        <br></br>
+                        <br></br>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <h3>Fourth Slide</h3>
+                        <br></br>
+                        <br></br>
                     </Carousel.Item>
                 </Carousel>
             </div>
