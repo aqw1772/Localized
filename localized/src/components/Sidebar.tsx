4cd3e7116748b5cceff9c78c99ddb22152905451
System Administrator <root@Anthonys-Work-MacBook-Pro.local>
2020-07-31 18:49:28 -0400

    Created span tags to represent labels (depicted in spec); added Satya.png and icon

--- localized/src/components/Sidebar.tsx
+++ localized/src/components/Sidebar.tsx
@@ -1,5 +1,9 @@
+import peopleSearch from "../images/PeopleSearch.png";
+import Satya from "../images/Satya.png";
 import React from 'react';
 import { pages } from "../App"; {/* Not sure if I need this */}
+
+
 
 //SideBar for the My Content page
 //The component will include buttons (eventually draggable) portraying user interest categories
@@ -14,7 +18,21 @@ class MyContentSidebar extends React.Component<IMyContentSidebarProps> {
     render() {
         return (
             <div className="myContentSidebar">
-                
+                <div className="nameAndPic">
+                    {/* Insert profile pic here */}
+                    <img src={Satya}></img>
+                    {/* Insert profile name here */}
+                    <h4>Satya Nadella</h4> {/* test */}
+                </div>
+                {/* Dropdown goes here */}
+                <span className="heading">
+                    <img src={peopleSearch}></img>
+                    Your Interests</span>
+                <div className="interests">
+                    <span className="school">School</span> 
+                    <span className="protest">Protest</span>
+                </div>
+                {/* Edit interests button */}
             </div>
         )
     };
--- localized/src/pages/MyContentPage.tsx
+++ localized/src/pages/MyContentPage.tsx
@@ -7,6 +7,7 @@ import CardDeck from 'react-bootstrap/CardDeck';
 import CardGroup from 'react-bootstrap/CardGroup';
 import CardColumns from 'react-bootstrap/CardColumns';
 import { BsNewspaper } from "react-icons/bs";
+import Sidebar from "../components/Sidebar";
 
 
 interface MyContentScreenProps {
@@ -22,65 +23,66 @@ export class MyContentPage extends React.Component<MyContentScreenProps> {
   render() {
     return (
       <div>
-        <button className="App-link" onClick={(e) => this.props.changePage(pages.EditCivilPage)}>
-            My Content Page
-        </button>
+        {/* NavBar goes here */}
+        <div className="myContentWrapper">
+          <button className="App-link" onClick={(e) => this.props.changePage(pages.EditCivilPage)}>
+              My Content Page
+          </button>
 
-        <p className="p"> 
-          Latest articles for you based on your interests <BsNewspaper/>
-          <hr></hr>
-        </p>
-    
-
+          <p className="p"> 
+            Latest articles for you based on your interests <BsNewspaper/>
+            <hr></hr>
+          </p>
       
-        <CardDeck className="card-deck">
-          <Card>
-            <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Election_voting_20180128.jpg/1200px-Election_voting_20180128.jpg"/>
-            <span className="voting">Voting</span>
-            <Card.Body>
-              <Card.Title><strong>Mail Voting gone wrong again -WSJ</strong></Card.Title>
-              <Card.Text>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facere minus nesciunt deleniti dolore error,
-                voluptate nostrum eligendi assumenda autem possimus nemo natus! Sit inventore dolor rem adipisci quas enim.
+          <Sidebar changePage={this.props.changePage} userInterests={"Implement redux here"}/> 
+        
+          <CardDeck className="card-deck">
+            <Card>
+              <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Election_voting_20180128.jpg/1200px-Election_voting_20180128.jpg"/>
+              <span className="voting">Voting</span>
+              <Card.Body>
+                <Card.Title><strong>Mail Voting gone wrong again -WSJ</strong></Card.Title>
+                <Card.Text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facere minus nesciunt deleniti dolore error,
+                  voluptate nostrum eligendi assumenda autem possimus nemo natus! Sit inventore dolor rem adipisci quas enim.
+                </Card.Text>
+                <Button variant="primary">Read More...</Button>
+              </Card.Body>
+            </Card>
+
+            <Card> 
+            <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/BostonSchoolSt3.JPG/1200px-BostonSchoolSt3.JPG"/>
+            <span className="school">Schools</span>
+              <Card.Body>
+                <Card.Title><strong>Schools close due to COVID-19:</strong></Card.Title>
+                <Card.Text>
+                  <div className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facere minus nesciunt deleniti dolore error,
+                  voluptate nostrum eligendi assumenda autem possimus nemo natus! Sit inventore dolor rem adipisci quas enim.{' '} </div>
+                </Card.Text>
+                <Button variant="primary">Read More...</Button>
+              </Card.Body>
+            </Card>
+
+            <Card>
+              <Card.Img variant="top" src="https://media1.s-nbcnews.com/j/newscms/2017_33/2124231/170817-black-lives-matter-protest-ac-926p_024173f5245dacd5c6cccbbed5fef4af.nbcnews-ux-2880-1000.jpg" />
+              <span className="protest">Protests</span>
+              <Card.Body>
+                <Card.Title><strong>Black Lives Matter Protests Continue </strong></Card.Title>
+                <Card.Text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facere minus nesciunt deleniti dolore error,
+                  voluptate nostrum eligendi assumenda autem possimus nemo natus! Sit inventore dolor rem adipisci quas enim.
               </Card.Text>
               <Button variant="primary">Read More...</Button>
-            </Card.Body>
-          </Card>
+              </Card.Body>
+            </Card>
+          </CardDeck>
 
-          <Card> 
-          <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/BostonSchoolSt3.JPG/1200px-BostonSchoolSt3.JPG"/>
-          <span className="school">Schools</span>
-            <Card.Body>
-              <Card.Title><strong>Schools close due to COVID-19:</strong></Card.Title>
-              <Card.Text>
-                <div className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facere minus nesciunt deleniti dolore error,
-                voluptate nostrum eligendi assumenda autem possimus nemo natus! Sit inventore dolor rem adipisci quas enim.{' '} </div>
-              </Card.Text>
-              <Button variant="primary">Read More...</Button>
-            </Card.Body>
-          </Card>
+          <div className="footer">
+            <p>&copy; New Technologists 2020 - Team 13</p>
+          </div>
 
-          <Card>
-            <Card.Img variant="top" src="https://media1.s-nbcnews.com/j/newscms/2017_33/2124231/170817-black-lives-matter-protest-ac-926p_024173f5245dacd5c6cccbbed5fef4af.nbcnews-ux-2880-1000.jpg" />
-            <span className="protest">Protests</span>
-            <Card.Body>
-              <Card.Title><strong>Black Lives Matter Protests Continue </strong></Card.Title>
-              <Card.Text>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facere minus nesciunt deleniti dolore error,
-                voluptate nostrum eligendi assumenda autem possimus nemo natus! Sit inventore dolor rem adipisci quas enim.
-            </Card.Text>
-            <Button variant="primary">Read More...</Button>
-            </Card.Body>
-          </Card>
-        </CardDeck>
-
-        <div className="footer">
-          <p>&copy; New Technologists 2020 - Team 13</p>
         </div>
-
-
-      
-       </div>
+      </div>
     );
   }
 }
