cb165d73662d5b5e96c835f0f2163f4526b4cea8
Angel Velazquez <v-angvel@microsoft.com>
2020-07-31 17:05:34 -0400

    Search bar cleared when entering search terms

--- localized/src/components/SearchBar.tsx
+++ localized/src/components/SearchBar.tsx
@@ -7,8 +7,9 @@ export class SearchBar extends React.Component<{}> {
     render() {
         return (
             <div className="SearchBar">
-                <form action="https://www.bing.com/search" onSubmit={this.onKeyUp} method="GET" target="_blank">
-                    <input style={{borderRadius: '10px', width: '250px'}} id="searchInput" type="text" name="q" placeholder="Search" />
+                <form id="TopSearchForm" action="https://www.bing.com/search" method="GET" target="_blank">
+                    <form onSubmit={this.onKeyUp} ><input style={{borderRadius: '10px', width: '250px'}} id="searchInput" type="text" name="q" placeholder="Search" /></form>
+                    
                 </form>
             </div >
         )
@@ -16,9 +17,23 @@ export class SearchBar extends React.Component<{}> {
 
     onKeyUp = (event: React.KeyboardEvent<HTMLFormElement>) => {
         console.log("Called");
-        if(event.key === "Enter") {
+        if(event.type === "submit") {
+            
+            let input = (document.getElementById("searchInput") as HTMLInputElement);
+            let searchTerm = input.value;
+
+            input.value = "";
+
+            // Submit the outer form using this 
+            let form = (document.getElementById("TopSearchForm") as HTMLFormElement);
+            let searchInput = (document.createElement("input") as HTMLInputElement);
+            searchInput.setAttribute('type', 'text');
+            searchInput.setAttribute('name', 'q');
+            searchInput.value = searchTerm;
+            form.appendChild(searchInput);
+            form.requestSubmit();
+            form.removeChild(searchInput);
+            event.preventDefault();
         }
-        // let input = await (document.getElementById("searchInput") as HTMLInputElement);
-        // input.value = "";
     }
 }
