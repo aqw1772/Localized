eecee128c40aaf577fb9b7a7f9f4e5c3219ba446
estheradediran <esther@Esthers-MBP.hsd1.tx.comcast.net>
2020-07-31 02:02:20 -0500

    tried adding font awesome icons...didn't work

--- localized/src/pages/MyContentPage.tsx
+++ localized/src/pages/MyContentPage.tsx
@@ -9,6 +9,8 @@ import CardColumns from 'react-bootstrap/CardColumns';
 
 
 
+
+
 interface MyContentScreenProps {
   changePage: (page: pages) => void;
 }
@@ -27,7 +29,7 @@ export class MyContentPage extends React.Component<MyContentScreenProps> {
         </button>
 
         <p className="p"> 
-          Latest articles for you based on your interests
+          Latest articles for you based on your interests 
           <hr></hr>
         </p>
     
@@ -37,19 +39,19 @@ export class MyContentPage extends React.Component<MyContentScreenProps> {
           <Card>
             <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Election_voting_20180128.jpg/1200px-Election_voting_20180128.jpg"/>
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
+              <Card.Title><strong>Mail Voting gone wrong again -WSJ</strong></Card.Title>
               <Card.Text>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facere minus nesciunt deleniti dolore error,
                 voluptate nostrum eligendi assumenda autem possimus nemo natus! Sit inventore dolor rem adipisci quas enim.
               </Card.Text>
-              <Button variant="primary" className="mt-5">Read More...</Button>
+              <Button variant="primary">Read More...</Button>
             </Card.Body>
           </Card>
 
           <Card> 
-          <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/BostonSchoolSt3.JPG/1200px-BostonSchoolSt3.JPG"  height="400"/>
+          <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/BostonSchoolSt3.JPG/1200px-BostonSchoolSt3.JPG"/>
             <Card.Body>
-              <Card.Title><strong>Card title </strong></Card.Title>
+              <Card.Title><strong>Schools close due to COVID-19:</strong></Card.Title>
               <Card.Text>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facere minus nesciunt deleniti dolore error,
                 voluptate nostrum eligendi assumenda autem possimus nemo natus! Sit inventore dolor rem adipisci quas enim.{' '}
@@ -61,7 +63,7 @@ export class MyContentPage extends React.Component<MyContentScreenProps> {
           <Card>
             <Card.Img variant="top" src="https://media1.s-nbcnews.com/j/newscms/2017_33/2124231/170817-black-lives-matter-protest-ac-926p_024173f5245dacd5c6cccbbed5fef4af.nbcnews-ux-2880-1000.jpg" />
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
+              <Card.Title><strong>Black Lives Matter Protests Continue </strong></Card.Title>
               <Card.Text>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facere minus nesciunt deleniti dolore error,
                 voluptate nostrum eligendi assumenda autem possimus nemo natus! Sit inventore dolor rem adipisci quas enim.
