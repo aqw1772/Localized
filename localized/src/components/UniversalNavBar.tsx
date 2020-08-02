61df0886dfe507cbf529a08af2af7896d994be35
Bunmi Ogidan <bunmiogidan@Bunmis-MBP.fios-router.home>
2020-08-02 16:36:54 -0400

    Imported navbar components for react-bootstrap

--- localized/src/components/UniversalNavBar.tsx
+++ localized/src/components/UniversalNavBar.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { pages } from "../App";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+
 
 interface UniversalNavBarProps {
     changePage: (page: pages) => void;
@@ -8,10 +13,19 @@ interface UniversalNavBarProps {
 class UniversalNavBar extends React.Component<UniversalNavBarProps>{
     render () {
         return(
-            <div className= "universalnav">
-                
-
-            </div>
+            <Navbar variant="dark" bg="dark" expand="lg" className="">
+            <Navbar.Brand >Simpler Styling</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link onClick={(e) => this.props.changePage(pages.Main)}>Civil Supporter</Nav.Link>
+                <Nav.Link onClick={(e) => this.props.changePage(pages.Button)}>My Content</Nav.Link>
+              </Nav>
+              <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              </Form>
+            </Navbar.Collapse>
+          </Navbar> 
 
         )
     }
