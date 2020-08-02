a633100a379d14c37807b3547d10bcb17a0320d6
Bunmi Ogidan <bunmiogidan@Bunmis-MBP.fios-router.home>
2020-08-02 16:46:13 -0400

    added nav pages when button is clicked

--- localized/src/components/UniversalNavBar.tsx
+++ localized/src/components/UniversalNavBar.tsx
@@ -10,16 +10,16 @@ interface UniversalNavBarProps {
     changePage: (page: pages) => void;
 }
 
-class UniversalNavBar extends React.Component<UniversalNavBarProps>{
-    render () {
+class UniversalNavBar extends React.Component<UniversalNavBarProps> {
+    render (){
         return(
             <Navbar variant="dark" bg="dark" expand="lg" className="">
             <Navbar.Brand >Simpler Styling</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
-                <Nav.Link onClick={(e) => this.props.changePage(pages.Main)}>Civil Supporter</Nav.Link>
-                <Nav.Link onClick={(e) => this.props.changePage(pages.Button)}>My Content</Nav.Link>
+                <Nav.Link onClick={(e) => this.props.changePage(pages.DesignCivilPage)}>Civil Supporter</Nav.Link>
+                <Nav.Link onClick={(e) => this.props.changePage(pages.MyContentPage)}>My Content</Nav.Link>
               </Nav>
               <Form inline>
                 <FormControl type="text" placeholder="Search" className="mr-sm-2" />
