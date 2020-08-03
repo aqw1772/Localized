105215d32f7dd50a2a3144ce682889f05f9ddbe1
Alex Wu <alexwu@Alexs-MacBook-Pro.local>
2020-08-02 21:57:24 -0500

    Fixed spacing between footer and sizing of local news card

--- localized/src/components/LocalNewsCard.tsx
+++ localized/src/components/LocalNewsCard.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 
 interface LocalNewsCardProps{
     image: string;
@@ -12,7 +11,7 @@ export class LocalNewsCard extends React.Component<LocalNewsCardProps>{
  render(){
      return(
         <Card className="LocalNewsCard">
-            <Card.Img variant="top" src={this.props.image}/>
+            <Card.Img className="LocalNewsPicture" variant="top" src={this.props.image}/>
             <span className="voting">Voting</span>
             <Card.Body className="TwoColGridCard">
               <Card.Title className="column"><strong>{this.props.title}</strong></Card.Title>
--- localized/src/components/LocalNewsDeck.tsx
+++ localized/src/components/LocalNewsDeck.tsx
@@ -11,8 +11,8 @@ export class LocalNewsDeck extends React.Component<{}>{
                     <div className="LocalNewsTitle">Catch up on your local news <MdLiveTv/></div>
                     <hr className="lineTitle"></hr>
                 </p>
-                <CardDeck className="card-deck">
-                    <LocalNewsCard image="https://www.gizmodo.com.au/content/uploads/sites/2/2020/07/07/a4myq463ikgrijkrl80o-732x411.jpg" title="Mail Voting gone wrong again -WSJ" caption= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facere minus nesciunt deleniti dolore error, voluptate nostrum eligendi assumenda autem possimus nemo natus! Sit inventore dolor rem adipisci quas enim." />
+                <CardDeck className="LocalNewsDeck">
+                    <LocalNewsCard image="https://www.gizmodo.com.au/content/uploads/sites/2/2020/07/07/a4myq463ikgrijkrl80o-732x411.jpg" title="Amazon pays federal income tax for the first time since 2016 -" caption= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facere minus nesciunt deleniti dolore error, voluptate nostrum eligendi assumenda autem possimus nemo natus! Sit inventore dolor rem adipisci quas enim." />
                     <LocalNewsCard image="https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Election_voting_20180128.jpg/1200px-Election_voting_20180128.jpg" title="Mail Voting gone wrong again -WSJ" caption= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facere minus nesciunt deleniti dolore error, voluptate nostrum eligendi assumenda autem possimus nemo natus! Sit inventore dolor rem adipisci quas enim." />
                 </CardDeck>
             </div>
